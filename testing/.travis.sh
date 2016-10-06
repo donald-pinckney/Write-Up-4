1a1b6a56f15ef46ccfc8d63dc4edf20a22a149f8
Donald Pinckney <donald_pinckney@icloud.com>
2016-10-05 23:29:18 -0700

    Update .travis.sh

--- testing/.travis.sh
+++ testing/.travis.sh
@@ -2,12 +2,6 @@
 
 # Don't call this script yourself, this is for Travis CI!
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    exit 0
-elif [ -z ${TRAVIS_PULL_REQUEST+x} ]; then
-    exit 0
-fi
-
 cd "$(dirname "$0")"/..
 
 testing/test_all.sh
