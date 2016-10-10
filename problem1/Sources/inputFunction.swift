9bab1fe6d52c5ed9f9c285864631c10b9f5c8ba5
Whyhow314 <whyhow3.141@gmail.com>
2016-10-10 00:32:14 +0000

    All functions and the gam as a whole works. Functions now have comments.

--- problem1/Sources/inputFunction.swift
+++ problem1/Sources/inputFunction.swift
@@ -1,3 +1,5 @@
+// This function prompts a user to input a letter and gives that letter as a character as output
+
 func readGuess() -> Character {
   print("Please enter a guess at a letter in the word below:")
   let guess = Character(readLine()!)
--- problem1/Sources/letterCheckingFunctions.swift
+++ problem1/Sources/letterCheckingFunctions.swift
@@ -1,3 +1,4 @@
+// This function takes a character and an array of characters as input, and outputs a bool describing whether the character is in the array
 func hasMatch(ofGuess: Character, inWord: [Character]) -> Bool {
   var isInWord = false
   for x in inWord {
@@ -9,6 +10,8 @@ func hasMatch(ofGuess: Character, inWord: [Character]) -> Bool {
   return isInWord
 }
 
+// This function takes as input two arrays of characters and a character
+// It outputs an array of characters that is based on the first array, but matching the second array in spaces where the second array contained the input character
 func update(currentInProgressWord: [Character], actualWord: [Character], guess: Character) -> [Character] {
   var updatedGuess = currentInProgressWord
   for x in 0..<actualWord.count {
@@ -19,6 +22,7 @@ func update(currentInProgressWord: [Character], actualWord: [Character], guess: 
   return updatedGuess
 }
 
+// This function takes an array of characters, and returns a bool representing whether the array matches an outside array
 func hasWon(_ inProgressWord: [Character]) -> Bool {
   return inProgressWord == randomWord
 }
