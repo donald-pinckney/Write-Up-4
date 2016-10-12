c26c74bba3dd05910a42e766e1f68e4c7d6354e0
DevBurrito <jishnu1@gmail.com>
2016-10-11 22:01:22 -0700

    finished

--- problem1/Sources/functions.swift
+++ problem1/Sources/functions.swift
@@ -1,8 +1,8 @@
 //This file contains all the functions necessary to run/compile main.swift
 
 //how far the user has gotten
-func inProgressWord(_ word: [Character]) {
-  for i in word {
+func printInProgressWord(_ word: [Character]) {
+  for i in 0..<word.count {
     print(word[i])
   }
 }
@@ -16,35 +16,44 @@ func printRemainingBodyParts(_ parts: Int) {
 }
 //takes in guess and returns it
 func readGuess() -> Character {
-  let input: Character = Character(readLine(!)!)
-  return input
+  let input = readLine()!
+  return Character(input)
 }
 //checks if guess is right
-func hasMatch(input: Character, word: String) -> Bool {
-  for character in word.characters {
-    if character == input {
+func hasMatch(ofGuess input: Character, inWord word: [Character]) -> Bool {
+  for i in 0..<word.count {
+    if word[i] == input {
       return true
     }
   }
   return false
 }
 //fixes '_' array
-func update(currentInProgressWord: [Character], actualWord: [Character], guess: Character)  {
+func update(currentInProgressWord: [Character], actualWord: [Character], guess: Character) -> [Character] {
   //temp arr to modify
-  newArr: [Character] = currentInProgressWord
+  var newArr: [Character] = currentInProgressWord
 
   for i in 0..<actualWord.count {
     if guess == actualWord[i] {
       newArr[i] = guess
     }
   }
+  return newArr
 }
 
 func printBadGuessMessage() {
   print("Wrong! There goes a body part! :(")
 }
 
-func hasWon(_ word: [String]) -> Bool
+func hasWon(_ word: [Character]) -> Bool {
+  for i in word {
+    if i == "_" {
+      return false
+    }
+  }
+
+  return true
+}
 
 func printWinMessage() {
   print("Congratulations, you win!")
