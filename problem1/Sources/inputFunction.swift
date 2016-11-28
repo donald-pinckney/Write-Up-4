b8b9e2176104ce950018931e97fe114f184f9e40
Whyhow314 <whyhow3.141@gmail.com>
2016-11-28 03:37:13 +0000

    Better input: no error when not 1 character

--- problem1/Sources/inputFunction.swift
+++ problem1/Sources/inputFunction.swift
@@ -2,6 +2,12 @@
 
 func readGuess() -> Character {
   print("Please enter a guess at a letter in the word below:")
-  let guess = Character(readLine()!)
-  return guess
+  let guess = String(readLine()!)!
+  if guess.characters.count == 1 {
+    return Character(guess)
+  }
+  else {
+    print("Please try again")
+    return readGuess()
+  }
 }
